7cfe6e724f665ccb0ff4353085e084a29ef9ca81
Lucas <lucas_linhares_@hotmail.com>
2022-12-29 14:16:01 -0300

    Instalando Router dom e MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Button } from '@mui/material'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+
+export const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route path = '/pagina-inicial' element = {<p><Button>Olá</Button></p>} />
+            <Route path = '*' element = {<Navigate to = '/pagina-inicial' />} />
+        </Routes>
+    )
+}
