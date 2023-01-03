1cc0bf90efd214eb86f3e79d6bd280f6ac3fc6e4
Lucas <lucas_linhares_@hotmail.com>
2023-01-02 22:36:58 -0300

    Estilizando barra de tarefas

--- src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper, TextField, useTheme } from '@mui/material';
+import { Button, Icon, Paper, TextField, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 
 
@@ -8,11 +8,30 @@ export const BarraDeFerramentas: React.FC = () => {
   const theme = useTheme();
 	
   return(
-    <Box component={Paper} display= 'flex' gap={1} height={theme.spacing(6)} marginX={1} padding={1} paddingX={2} alignItems= 'center' >
+    <Box
+      component={Paper}
+      display= 'flex'
+      gap={1}
+      height={theme.spacing(6)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      alignItems= 'center' 
+    >
 
-      <TextField size='small'/>
+      <TextField size='small' placeholder='Pesquisar...'/>
 
-      <Button>Novo</Button>
+      <Box display='flex' flex={1} justifyContent= 'end'>
+        <Button
+          variant='contained'
+          color='primary'
+          disableElevation
+          endIcon={<Icon>add</Icon>}
+        >
+					Novo
+        </Button>
+      </Box>
+      
 
     </Box>
   );
